c9943ba8ba96d5fc801042a0b6ea56f79196f0e9
Parabeac-Nest <admin@parabeac.com>
2022-12-12 05:52:13 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/abo_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:abo/theme/abo_text_styles.g.dart';
+import 'package:abo/theme/abo_colors.g.dart';
+
+class AboTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: AboTextStyles.headline1,headline2: AboTextStyles.headline2,headline3: AboTextStyles.headline3,headline4: AboTextStyles.headline4,headline5: AboTextStyles.headline5,headline6: AboTextStyles.headline6,subtitle1: AboTextStyles.subtitle1,subtitle2: AboTextStyles.subtitle2,bodyText1: AboTextStyles.bodyText1,bodyText2: AboTextStyles.bodyText2,button: AboTextStyles.button,caption: AboTextStyles.caption,overline: AboTextStyles.overline,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: AboColors.lightOnPrimary,primary: AboColors.lightPrimary,primaryContainer: AboColors.lightPrimaryContainer,secondary: AboColors.lightSecondary,onSecondary: AboColors.lightOnSecondary,onPrimaryContainer: AboColors.lightOnPrimaryContainer,secondaryContainer: AboColors.lightSecondaryContainer,onSecondaryContainer: AboColors.lightOnSecondaryContainer,onTertiary: AboColors.lightOnTertiary,tertiary: AboColors.lightTertiary,tertiaryContainer: AboColors.lightTertiaryContainer,onTertiaryContainer: AboColors.lightOnTertiaryContainer,error: AboColors.lightError,onError: AboColors.lightOnError,errorContainer: AboColors.lightErrorContainer,onErrorContainer: AboColors.lightOnErrorContainer,background: AboColors.lightBackground,onBackground: AboColors.lightOnBackground,surface: AboColors.lightSurface,onSurface: AboColors.lightOnSurface,surfaceVariant: AboColors.lightSurfaceVariant,onSurfaceVariant: AboColors.lightOnSurfaceVariant,outline: AboColors.lightOutline,inverseSurface: AboColors.lightInverseSurface,onInverseSurface: AboColors.lightOnInverseSurface,inversePrimary: AboColors.lightInversePrimary,surfaceTint: AboColors.lightSurfaceTint);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: AboColors.darkPrimary,onPrimary: AboColors.darkOnPrimary,primaryContainer: AboColors.darkPrimaryContainer,onPrimaryContainer: AboColors.darkOnPrimaryContainer,secondary: AboColors.darkSecondary,onSecondary: AboColors.darkOnSecondary,secondaryContainer: AboColors.darkSecondaryContainer,onSecondaryContainer: AboColors.darkOnSecondaryContainer,tertiary: AboColors.darkTertiary,onTertiary: AboColors.darkOnTertiary,tertiaryContainer: AboColors.darkTertiaryContainer,onTertiaryContainer: AboColors.darkOnTertiaryContainer,error: AboColors.darkError,onError: AboColors.darkOnError,errorContainer: AboColors.darkErrorContainer,onErrorContainer: AboColors.darkOnErrorContainer,background: AboColors.darkBackground,onBackground: AboColors.darkOnBackground,surface: AboColors.darkSurface,onSurface: AboColors.darkOnSurface,surfaceVariant: AboColors.darkSurfaceVariant,outline: AboColors.darkOutline,inverseSurface: AboColors.darkInverseSurface,inversePrimary: AboColors.darkInversePrimary,surfaceTint: AboColors.darkSurfaceTint,onSurfaceVariant: AboColors.darkOnSurfaceVariant,onInverseSurface: AboColors.darkOnInverseSurface);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
